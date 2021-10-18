d43c35f8b123655aaa7a12b927be7ee774077154
Stevhen <stevhen.work@gmail.com>
2021-10-18 18:17:16 +0700

    Add QuickSort Algorithm in Go

---
+++ Sorting/QuickSort.go
@@ -0,0 +1,54 @@
+//
+//  QuickSort.go
+//  QuickSort
+//
+//  Created by Stevhen on 18/10/21.
+//
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func swap(a *int64, b *int64) {
+	t := *a
+	*a = *b
+	*b = t
+}
+
+func partition(arr []int64, low int, high int) int {
+	pivot := arr[high]
+	i := low
+	for j := low; j <= high; j++ {
+		if arr[j] < pivot {
+			swap(&arr[i], &arr[j])
+			i++
+		}
+	}
+	swap(&arr[i], &arr[high])
+	return i
+}
+
+func quicksort(arr []int64, low int, high int) {
+	if low < high {
+		pi := partition(arr, low, high)
+		quicksort(arr, low, pi-1)
+		quicksort(arr, pi+1, high)
+	}
+}
+
+//Example
+func main() {
+	//Generates n random values and sort with quicksort
+	n := 100
+
+	arr := make([]int64, 100)
+	for i := 0; i < n; i++ {
+		arr[i] = rand.Int63n(100)
+	}
+
+	quicksort(arr, 0, len(arr)-1)
+	fmt.Printf("%v\n", arr)
+}
